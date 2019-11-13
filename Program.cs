7c3c7f1defeea29929cb4895a952b2d9237e74d6
Dan Hashmi <psydh5@nottingham.ac.uk>
2019-11-13 11:46:02 +0000

    Added basic loops

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace test
+{
+    class Program: TankResidents
+    {
+        static void Main(string[] args)
+        {
+        string userInput;
+        string userInput1;
+        float intVal;
+        int Points;
+        ConsoleKeyInfo confirm;
+        do
+        {
+            Console.Write ( "Enter number of White Minnows: " );
+            userInput = Console.ReadLine ();
+            //intVal = Convert.ToInt32(userInput);
+            bool result = Single.TryParse ( userInput, out intVal);
+            if ( result )
+            {
+                int Val1 = Convert.ToInt32(intVal);
+                Points = Val1 * WhiteMinnow;
+                
+                Console.WriteLine ( "Fish Points: " + Points );
+            } else {
+                Console.WriteLine ( "Please enter a number" );
+                break;
+            }
+            bool check = false;
+            do {
+                Console.WriteLine ( "Type 'no' to change number of fish, otherwise type 'yes' to continue" );
+                confirm = Console.ReadKey ( true );
+                check = !( ( confirm.Key == ConsoleKey.Y ) || ( confirm.Key == ConsoleKey.N ) );
+            } while ( check );
+            switch ( confirm.Key )
+            {
+                case ConsoleKey.N:; 
+                break;
+                 
+                case ConsoleKey.Y: Console.WriteLine ( "Continued:" ); 
+                Console.WriteLine("Enter Aquarium type (A or B?): ");
+                userInput1 = Console.ReadLine();
+                string userinputcaps = userInput1.ToUpper();
+                Console.WriteLine($"You entered {userinputcaps}");
+                int Val1 = Convert.ToInt32(intVal);
+                Points = Val1 * WhiteMinnow;
+                if(userinputcaps == "A")
+                {
+                    if(Points <= AquariumA)
+                    {
+                        Console.WriteLine("Aquarium not overcrowded");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Overcrowded");
+                    }
+                }
+                else if(userinputcaps == "B")
+                {
+                    if(Points <= AquariumB)
+                    {
+                        Console.WriteLine("Aquarium not overcrowded");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Overcrowded");
+                    }
+                }
+
+                else 
+                {
+                    Console.WriteLine("Aquarium not recognsied. Please start again.");
+                }
+                break;
+            } 
+        } while ( confirm.Key != ConsoleKey.Y );
+        }   
+    }
+}
---
+++ TankResidents.cs
@@ -0,0 +1,34 @@
+namespace test
+{
+    public class TankResidents : Aquariums
+    {
+        public static int TempRosyBarb = 3;
+        //amber traffic light
+        public static int TropRosyBarb = 3;
+        //amber traffic light
+        public static int AmanoShrimp = 1;
+        //green traffic light
+        public static int FreshwaterShrimp = 0;
+        //green traffic light
+        public static int WhiteMinnow = 1;
+        //green traffic light
+        public static int GoldMinnow = 1;
+        //green traffic light
+        public static int AssortedDanio = 1;
+        //green traffic light
+         public static int RedSpottedSnail = 0;   
+        //green traffic light
+        public static int RedLipsSnail = 0;   
+        //green traffic light
+        public static int SpiralHornSnail = 0;   
+        //green traffic light
+        public static int SuckingLoach = 4;
+        //amber traffic light
+        public static int TempSuckingLoach = 4;
+        //amber traffic light
+        public static int VariatusPlaty = 2;
+        //green traffic 
+        public static int BlackCorydoras = 4;
+        //green traffic light
+    }
+}
